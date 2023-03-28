cf72ed4feea00548418f101e124772c5a615ff8d
Максим Мазур <1mazurmaxim12@gmail.com>
2023-03-28 11:30:47 +0300

    add 3 classes

---
+++ Lottery.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeWork_6_lottery_
+{
+    public static class Lottery
+    {
+        public static List<int> Emit(int number)   // повертає list унікальних чисел від 1 до number
+        {
+            var lotteryNumbers = new List<int>();
+            for (int i = 1; i <= number; i++)
+            {
+                lotteryNumbers.Add(i);
+            }
+            var random = new Random();   //Random - потрібен для генерування випадкових чисел
+            for (int i = lotteryNumbers.Count - 1; i > 0; i--) // зворотній цикл
+            {
+                int j = random.Next(i + 1);   // j - випадкове число
+                int temp = lotteryNumbers[i];
+                lotteryNumbers[i] = lotteryNumbers[j];
+                lotteryNumbers[j] = temp;
+            }
+            return lotteryNumbers;
+        }
+    }
+}
